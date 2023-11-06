2bff206111543f706d43a8ac80ff64715181d243
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-11-06 12:38:04 -0700

    Added user routes as well as controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,121 @@
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models')
 
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+
+            const userObj = {
+                users,
+            };
+
+            res.json(userObj);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-_v');
+            if (!user) {
+                return res.status(404).json({ message: 'No user!' })
+            }
+
+            res.json({
+                user,
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async addUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(400).json({ message: 'No user!' });
+            }
+
+            res.json({ message: 'User deleted.' })
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user!' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async addFriend(req, res) {
+        console.log('You are adding a friend');
+        console.log(req.body);
+
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { assignments: req.body } },
+                { runValidators: true, new: true },
+            );
+
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({ message: 'No user!' });
+            }
+
+            res.json(user);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+      },
+
+      async deleteFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId },
+                { $pull: {assignment: { assignmentId: req.parmams.assignmentId} } },
+                {runValidators: true, new: true}
+            );
+
+            if (!user) {
+                return res 
+                    .status(404)
+                    .json({ message: 'No user!'});
+            }
+
+            res.json(user);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+      },
+};
